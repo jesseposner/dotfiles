53bb264444e83234904281c4b2d3a254bf422c75
Jesse Posner <jesse.posner@gmail.com>
2026-02-08 23:39:54 -0800

    Modernize shell, terminal, and tmux configs
    - Alacritty: migrate yml to toml, fix deprecated option_as_alt,
      add OneDark bright colors and search/vi mode colors
    - Fish: clean up config, add Starship prompt, add OneDark syntax
      theme and rustup conf.d files
    - Starship: add shell name indicator via env_var module
    - Tmux: drop tmux-sensible and tmux-onedark-theme plugins, inline
      sensible settings, port OneDark theme to modern -style syntax
    - Add bash/zsh configs with Starship prompt and EDITOR=nvim

---
+++ dot_config/private_fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,29 @@
+# OneDark theme for fish syntax highlighting
+set --global fish_color_normal ABB2BF
+set --global fish_color_command 61AFEF
+set --global fish_color_param ABB2BF
+set --global fish_color_keyword C678DD
+set --global fish_color_quote 98C379
+set --global fish_color_redirection 56B6C2 --bold
+set --global fish_color_end 56B6C2
+set --global fish_color_comment 5C6370 --italics
+set --global fish_color_error E06C75
+set --global fish_color_escape E5C07B
+set --global fish_color_operator 56B6C2
+set --global fish_color_autosuggestion 4B5263
+set --global fish_color_cancel -r
+set --global fish_color_cwd 98C379
+set --global fish_color_cwd_root E06C75
+set --global fish_color_host ABB2BF
+set --global fish_color_host_remote E5C07B
+set --global fish_color_user 98C379
+set --global fish_color_status E06C75
+set --global fish_color_valid_path --underline
+set --global fish_color_history_current --bold
+set --global fish_color_search_match --background=3E4452
+set --global fish_color_selection --bold --background=3E4452
+set --global fish_pager_color_completion ABB2BF
+set --global fish_pager_color_description 5C6370 --italics
+set --global fish_pager_color_prefix 61AFEF --bold --underline
+set --global fish_pager_color_progress ABB2BF --background=3E4452
+set --global fish_pager_color_selected_background --background=3E4452
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,28 +1,30 @@
-# fish greeting
-set -x fish_greeting ""
+# Suppress greeting
+set -g fish_greeting
 
-# fisher plugins
-if not functions -q fisher
-  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-  fish -c fisher
-end
-
-# set spacefish prompts
-set SPACEFISH_PROMPT_ORDER time user dir host git package node ruby golang php rust haskell julia docker aws venv conda pyenv dotnet kubecontext exec_time line_sep battery jobs exit_code char
-
-# set default editor
+# Default editor
 set -gx EDITOR nvim
-
-# load rbenv
-status --is-interactive; and source (rbenv init -|psub)
-
-# pyenv
-status is-login; and pyenv init --path | source
-pyenv init - | source
 
 # vi mode
 fish_vi_key_bindings
 
-# disable blinking cursor
+# Disable blinking cursor
 set fish_cursor_unknown block
+
+# Homebrew
+eval (/opt/homebrew/bin/brew shellenv)
+
+# PATH
+fish_add_path $HOME/.local/bin
+fish_add_path "$HOME/.volta/bin"
+set -gx VOLTA_HOME "$HOME/.volta"
+
+# rbenv
+status --is-interactive; and source (rbenv init -|psub)
+
+# Cargo/Rust (loaded via conf.d/rustup.fish)
+
+# OrbStack
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# Starship prompt
+starship init fish | source
