ed04d020a8735592abb0eb0787a3133925fe0ef4
Jesse Posner <jesse.posner@gmail.com>
2026-02-09 03:49:49 -0800

    Add Ctrl-b keybinding for fzf-lua buffers

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -5,6 +5,7 @@ return {
     { "<leader>f", "<cmd>FzfLua files<CR>", desc = "Find files" },
     { "<leader>p", "<cmd>FzfLua files<CR>", desc = "Find files" },
     { "<leader>b", "<cmd>FzfLua buffers<CR>", desc = "Buffers" },
+    { "<C-b>", "<cmd>FzfLua buffers<CR>", desc = "Buffers" },
     { "<leader>g", "<cmd>FzfLua live_grep<CR>", desc = "Live grep" },
   },
 }
